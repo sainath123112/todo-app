8ff7916266045e28957142951fe21ca18e0c3a81
sainath123112 <113941618+sainath123112@users.noreply.github.com>
2023-03-02 13:25:52 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
 import './App.css';
 
+ function Todos(props)
+ {
+    function removeItem(e)
+    {
+      props.setName(props.todoArray.filter((el)=>el.id!==props.todo.id));
+    }
+
+    function doneItem(e)
+    {
+      const todo_item = e.target.parentElement.parentElement;
+      todo_item.classList.toggle("todo-item-done");  
+      if(todo_item.classList.contains("todo-item-done"))
+      {
+        setBtndone("Undo");
+      }
+      else 
+      {
+        setBtndone("Done");
+      }
+      
+    }
+const [btndone, setBtndone] = useState("Done");
+
+  return(
+    <div className="todo-item">
+      <h1>{props.namehead}</h1>
+      <div className="todo-item-btns">
+      <button className="todo-remove" onClick={removeItem}>Remove</button>
+      <button className="todo-done" onClick={doneItem}>{btndone}</button>
+      </div>
+
+    </div>
+  );
+ }
+
+
 function App() {
+
+  const [newName, setNewname] = useState("");
+  const [name, setName] = useState(()=>
+       {
+        const savedTodos = localStorage.getItem("name");
+        if (savedTodos) {
+          // return the parsed the JSON object back to a javascript object
+          return JSON.parse(savedTodos);
+          // otherwise
+        } else {
+          // return an empty array
+          return [];
+        }
+       }
+  );
+  useEffect(()=>
+  {
+    if(name.length===0)
+  {
+    document.querySelector(".clear-all").style.display="none";
+  }
+  localStorage.setItem("name", JSON.stringify(name));
+  }, [name]);
+  
+
+
+  function addtodo()
+  {
+    if(newName)
+    {
+      setName([...name,{
+        task:newName,
+        id:name.length+1}]);
+      setNewname("");
+      document.querySelector(".clear-all").style.display="block";
+    
+    const inputChng = document.querySelector(".todo-input");
+    inputChng.value = "";
+      }
+  }
+
+  function getEnteredtodo(e)
+  {
+    setNewname(e.target.value);
+    
+  }
+function clearAll()
+{
+  setName([]);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="todo-heading">Todo List</h1>
+      <div className="todo-enter">
+        <input className="todo-input" type="text" onChange={getEnteredtodo} placeholder="Enter your todo..!"/>
+        <button className="todo-add" onClick={addtodo}>ADD</button>
+      </div>
+      <div className="todo-list">
+        {name.map((val)=><Todos key={val.id} namehead={val.task} todoArray={name} todo={val} setName={setName}></Todos>).reverse()}
+      </div>
+      <button className="clear-all" onClick={clearAll}>clear all</button>
+      
     </div>
   );
 }
